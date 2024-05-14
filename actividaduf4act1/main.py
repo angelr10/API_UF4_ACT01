fea00b10e0a835b3389f3c6ad77ad2aa1fb1887f
angelr10 <andreh10155@gmail.com>
2024-05-14 23:50:30 +0200

    agreeeegando contenido actual

--- actividaduf4act1/main.py
+++ actividaduf4act1/main.py
@@ -20,29 +20,39 @@ class product(BaseModel):
 def read_root():
 	return{"Esta pagina funciona"}
 
-
-
-# @app.get("/contenido/categorias")
-# def muestraCategorias():
-# 	contenido = general_db.showTablaWithName("category")
-# 	return contenido
-
-# @app.get("/contenido/subcategorias")
-# def muestraSubCategorias():
-# 	contenido = general_db.showTablaWithName("subcategory")
-# 	return contenido
-
 @app.get("/product/")
-def muestraProductos(): #retorna una lista json de toda la lista productos
+def muestraProductos():
 	contenido = product_db.showTablaWithName("product")
 	return contenido
 
 @app.get("/product/{id}")
-def muestraUnProducto(id: int): #retorna una lista json de toda la lista productos
+def muestraUnProducto(id: int): 
 	contenido = product_db.contenidoProductoID(id)
 	return contenido
 
 @app.post("/product/")
 def insertaUnProducto(producto: product):
 	contenido = product_db.insertaProduct(producto)
+	return contenido
+
+@app.put("/product/producte/{id}/{precio}")
+def editaProductoConId(id: int,precio: int):
+	contenido = product_db.modificaWithId(id,precio)
+	return contenido
+
+@app.delete("/product/{id}")
+def deleteProducto(id: int):
+	contenido = product_db.eliminaProducto(id)
+	return contenido
+
+@app.get("/productAll")
+def muestraTodoFormateado():
+	contenido = product_db.productosSeccionados()
+	return contenido
+
+
+#ACT 02
+@app.post("/loadProducts")
+def cargaMasiva():
+	contenido = product_db.cargaMasivaTodo()
 	return contenido
--- actividaduf4act1/product_db.py
+++ actividaduf4act1/product_db.py
@@ -1,5 +1,5 @@
 from client import db_client
-#01 product/
+#01 product/ XX
 def showTablaWithName(tablaLectura):
 	conexion = db_client()
 	cursor = conexion.cursor()
@@ -12,7 +12,8 @@ def showTablaWithName(tablaLectura):
 
 	conexion.close()
 	return listProductos
-#02 product/id
+
+#02 product/id XX
 def contenidoProductoID(id):
 	conexion = db_client()
 	cursor = conexion.cursor()
@@ -26,24 +27,81 @@ def contenidoProductoID(id):
 
 	conexion.close()
 	return listProductos
-#03 product/ post
+#03 product/ post XX
 def insertaProduct(prod):
 	conexion = db_client()
 	cursor = conexion.cursor()
-	query = "insert into botiga.product (name,description,company,price,units,subcategory_id,created_at,updated_at) VALUES (%s,%s,%s,%s,%s,%s,NOW(),NOW())"
-	cursor.execute(query,(prod.name,prod.description,prod.company,prod.price,prod.units,prod.subcategory_id))
-	conexion.commit()
+	query = "INSERT into botiga.product (name,description,company,price,units,subcategory_id,created_at,updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s,%s);"
+	cursor.execute(query,(prod.name,prod.description,prod.company,prod.price,prod.units,prod.subcategory_id,prod.created_at,prod.updated_at))
 	
+	queryD = "select * from botiga.product where product_id = %s;"
 	ultimaId = cursor.lastrowid
-	cursor.execute("select * from botiga.product where product_id = %s;",(ultimaId,))
+	conexion.commit()
+
+	cursor.execute(queryD,(ultimaId,))
 	producto_insertado = cursor.fetchone()
 	conexion.close()
-	conexion.close()
+	cursor.close()
+	print(ultimaId)
 
 	if producto_insertado:
 		return productoDict(producto_insertado)
 	else:
-		return "error producto no se ha podido insertar"
+		return "error producto no se ha podido insertar:"
+#Modifica por id XX
+def modificaWithId(id,preu):
+	conexion = db_client()
+	cursor = conexion.cursor()
+	query = "UPDATE product SET price = %s WHERE product_id = %s;"
+	cursor.execute(query,(preu,id))
+
+	query ="select * from product WHERE product_id= %s;"
+	cursor.execute(query,(id,))
+	resultado = cursor.fetchone()
+	return productoDict(resultado)
+
+#Muestra todo seccionado join
+def productosSeccionados():
+	conexion = db_client()
+	cursor = conexion.cursor()
+	query = """
+		SELECT 
+            p.id AS product_id,
+            p.name AS product_name,
+            s.name AS subcategory_name,
+            c.name AS category_name,
+            p.brand,
+            p.price
+        FROM 
+            product p
+        JOIN 
+            subcategory s ON p.subcategory_id = s.id
+        JOIN 
+            category c ON s.category_id = c.id;
+	"""
+	cursor.execute(query)
+	resultado = cursor.fetchall()
+	listProductos =[]
+	
+	for a in resultado:
+		aDict = productoDict(a)
+		listProductos.append(aDict)
+	conexion.close()
+	return listProductos
+
+#Delete XX
+def eliminaProducto(id):
+	conexion = db_client()
+	cursor = conexion.cursor()
+	query= "select * from product WHERE product_id= %s;"
+	cursor.execute(query,(id,))
+	resultado = cursor.fetchone()
+
+
+	query= "delete from product where product_id=%s;"
+	cursor.execute(query,(id,))
+	conexion.close()
+	return productoDict(resultado)
 
 def productoDict(a):
 	return {"product_id":a[0],
@@ -57,3 +115,6 @@ def productoDict(a):
          "updated_at":a[8]
     }
 	
+#Actividad02
+def cargaMasivaTodo():
+	pass
